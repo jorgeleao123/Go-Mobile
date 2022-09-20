209e5fbaf5b6099b2145c0a5b411a2e43e5fb86e
Christian Hitoshi <tetihamai@gmail.com>
2022-09-19 21:00:04 -0300

    Tela inicial finalizada (sem integração))

--- go-app/app/src/main/java/com/example/go_app/MainActivity.kt
+++ go-app/app/src/main/java/com/example/go_app/MainActivity.kt
@@ -4,15 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.go_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnJaTenhoConta.setOnClickListener {
+            goToLogin()
+        }
 
     }
 
-    fun goToLogin(v: View){
+    fun goToLogin(){
         val telaCadastro = Intent(this, Cadastro1::class.java)
         startActivity(telaCadastro)
     }
