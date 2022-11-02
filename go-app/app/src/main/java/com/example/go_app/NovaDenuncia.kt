2f7db5fcea50c1966ded8b4cbd5f9ce17da5c08e
Christian <tetihamai@gmail.com>
2022-11-01 21:03:50 -0300

    Telas de formulários feitos (visual)

---
+++ go-app/app/src/main/java/com/example/go_app/NovaDenuncia.kt
@@ -0,0 +1,11 @@
+package com.example.go_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NovaDenuncia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nova_denuncia)
+    }
+}
