6eee4558d108542026606d9d066abfc43719ccf2
taisfermino <tais.souza@bandtec.com.br>
2022-11-08 20:35:34 -0300

    Refactor: recyclerView notifications

--- go-app/app/src/main/java/com/example/go_app/Notification.kt
+++ go-app/app/src/main/java/com/example/go_app/Notification.kt
@@ -2,10 +2,27 @@ package com.example.go_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.go_app.adapter.CustomAdapterNotification
 
 class Notification : AppCompatActivity() {
+    var recyclerView: RecyclerView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
+
+        recyclerView = findViewById(R.id.recyclerView)
+
+        //configurar adapter
+        val adapter = CustomAdapterNotification()
+
+        //configurar RecyclerView
+
+        //configurar RecyclerView
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+        recyclerView?.layoutManager = layoutManager
+        recyclerView?.setHasFixedSize(true)
+        recyclerView?.adapter = adapter
     }
 }
