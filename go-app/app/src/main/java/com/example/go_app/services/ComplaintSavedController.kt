4e952d963ae192f385458902846461f9609c4788
Pedro Aragão <pedro.aragao@valemobi.com.br>
2022-11-17 19:52:05 -0300

    requisição itens salvos

--- go-app/app/src/main/java/com/example/go_app/services/ComplaintSavedController.kt
+++ go-app/app/src/main/java/com/example/go_app/services/ComplaintSavedController.kt
@@ -1,10 +1,22 @@
 package com.example.go_app.services
 
+import com.example.go_app.models.ComplaintsResponse
 import com.example.go_app.models.SaveComplaint
-import retrofit2.http.Body
-import retrofit2.http.POST
+import retrofit2.Call
+import retrofit2.http.*
 
 interface ComplaintSavedController {
     @POST("/complaint-saved")
-    fun login(@Body body: SaveComplaint)
+    fun saveComplaint(@Body body: SaveComplaint)
+
+    @GET("/complaint-saved/{userId}")
+    fun getComplaintSave(
+        @Path("userId") userId: Int,
+    ): Call<List<ComplaintsResponse>>
+
+    @DELETE("/complaint-saved/{userId}/{complaintId}")
+    fun removeComplaintSave(
+        @Path("userId") userId: Int,
+        @Path("complaintId") complaintId: Int,
+    )
 }
