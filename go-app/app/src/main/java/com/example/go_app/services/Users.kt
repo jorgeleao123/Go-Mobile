9ace467e27b383fa43ef9a7a63f2805304faf5ed
Christian <tetihamai@gmail.com>
2022-11-27 19:41:21 -0300

    Integrando telas

--- go-app/app/src/main/java/com/example/go_app/services/Users.kt
+++ go-app/app/src/main/java/com/example/go_app/services/Users.kt
@@ -1,5 +1,7 @@
 package com.example.go_app.services
 
+import com.example.go_app.models.UserAttRequest
+import com.example.go_app.models.UserAttResponse
 import com.example.go_app.models.SuccessResponse
 import com.example.go_app.models.UserRequest
 import com.example.go_app.models.UserResponse
@@ -7,8 +9,8 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface Users {
 
@@ -22,6 +24,12 @@ interface Users {
         @Path("email") email: String,
         @Path("password") password: String,
         ): Call<UserResponse>
+
+    @PUT("/users/{id}")
+    fun attUser(
+        @Path("id") id: Int,
+        @Body body: UserAttRequest,
+    ): Call<UserAttResponse>
 
     @DELETE("/users/{id}")
     fun deleteUser(
